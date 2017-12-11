ba024ff51cfd7facbe36e392b7b261952ab5d3f0
Matt Redmon <matt.redmon@live.com>
2017-12-11 00:16:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+logShout('HELLO')
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+logWhisper('hello')
