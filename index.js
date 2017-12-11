4cf39e29c40fdae43740174706aa20e0020cc2ed
Matt Redmon <matt.redmon@live.com>
2017-12-11 00:31:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ logWhisper('hello')
 function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
     return "YES INDEED!"
-  } else if (string.toLowerCase() === string) {
-    console.log('I can\'t hear you!');
+  if (string.toLowerCase() === string) {
+    return
   }
 }
 sayHiToGrandma('HELLO')
