58bd07e124009523da998e12c24656ffacdaa4c0
Matt Redmon <matt.redmon@live.com>
2017-12-11 00:32:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
     return "YES INDEED!"
   if (string.toLowerCase() === string) {
-    return
+    return 'I can\'t hear you!'
   }
 }
 sayHiToGrandma('HELLO')
